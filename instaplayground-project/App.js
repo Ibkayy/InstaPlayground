14a22492ba182c6504cda54802e631f14c620d42
Ibkayy <ibukunomojolz@gmail.com>
2017-12-07 13:59:27 +0100

    added background image, instagram logo to screen and made the logo resize itself and scroll with the screen

--- instaplayground-project/App.js
+++ instaplayground-project/App.js
@@ -1,21 +1,64 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { Text, View, ImageBackground, Image, StatusBar, ScrollView } from 'react-native';
+import Dimensions from 'Dimensions';
 
-export default class App extends React.Component {
+//code gives us the width and height of the current screen
+const windowSize = Dimensions.get('window');
+
+export default class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+
+    }
+  }
+
+  loginScreenComponent = () => {
+    return (
+      <ImageBackground
+       style={viewStyles.container}
+       resizeMode={'cover'}
+       source={require('./src/images/instabackground1.jpg')}
+      >
+
+        <StatusBar
+          barStyle={'light-content'}
+        />
+
+        <ScrollView style={viewStyles.scrollViewStyle}>
+          <Image
+            style={viewStyles.instagramLogo}
+            resizeMode={'contain'}
+            source={require('./src/images/instagram-text-logo.png')}
+          />
+        </ScrollView>
+
+      </ImageBackground>
+    );
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+      this.loginScreenComponent()
     );
   }
 }
 
-const styles = StyleSheet.create({
+const viewStyles = ({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'transparent',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  instagramLogo: {
+    width: (0.45 * windowSize.width),
+    height: (0.15 * windowSize.height),
+    marginBottom: 25,
+    alignSelf: 'center'
+  },
+  scrollViewStyle: {
+    paddingTop: '30%'
+  }
 });
